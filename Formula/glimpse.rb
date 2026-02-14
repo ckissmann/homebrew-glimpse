7a743e0c249b6fc77a95efc27840e9418cb65406
Christoph Kissmann <72171282+kissmannchristoph@users.noreply.github.com>
2026-02-14 05:49:27 +0100

    Update glimpse.rb

--- Formula/glimpse.rb
+++ Formula/glimpse.rb
@@ -7,10 +7,10 @@ class Glimpse < Formula
    on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ckissmann/glimpse/releases/download/v2026.2.14.17/glimpse-1.0.0-macos-arm"
-      sha256 "sha256:b5fdfa258eb9c30ecaa9cf2df2134ad55933c2970ba0bb32767fe8716270f495"
+      sha256 "b5fdfa258eb9c30ecaa9cf2df2134ad55933c2970ba0bb32767fe8716270f495"
     else
       url "https://github.com/ckissmann/glimpse/releases/download/v2026.2.14.17/glimpse-1.0.0-macos-intel"
-      sha256 "sha256:11d8808572f96de5426255ee0de8d28f8dd36e80c8029aa2b7a7ce30b0888629"
+      sha256 "11d8808572f96de5426255ee0de8d28f8dd36e80c8029aa2b7a7ce30b0888629"
     end
   end
 
